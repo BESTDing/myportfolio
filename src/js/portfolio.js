9a56cdc1282d2c0f6e7f292e8a975ac5e656fe20
BESTDing <estding0@gmail.com>
2017-07-22 12:16:56 +0800

    修改浏览器滚动条使用niceScroll，添加jquery

--- src/js/portfolio.js
+++ src/js/portfolio.js
@@ -1,5 +1,9 @@
 window.onload = function () {
   addEvent();
+  /*
+   *初始化niceScroll
+   */
+  $('body').niceScroll();
 }
 
 function addEvent() {
@@ -75,17 +79,14 @@ function scrollToElement(event) {
   queryString = queryString.toLowerCase();
   // console.log(queryString);
   if (queryString === 'home') {
-    scroll_To(0);
+    $('body').getNiceScroll().doScrollPos(0, 0);
     return;
   }
   console.log(queryString);
   var em = window.document.querySelectorAll('.' + queryString)[0];
   console.log(em);
   var box = getElementBoundingInDocument(em);
-  if (box.y > 898)
-    scroll_To(897);
-  else
-  scroll_To(box.y);
+  $('body').getNiceScroll().doScrollPos(0, box.y);
   if (ISDISPLAY) {
     toggleSlide();
   }
@@ -99,38 +100,3 @@ function temHandler(event) {
   console.log(event);
 }
 
-
-function scroll_To(tar_y){ //tar_y 即滑动条顶端 距离页面最上面的距离
-  //console.log('oooooooo')
-  var timer = setTimeout(function(){
-    var current_y = document.body.scrollTop
-    //console.log(current_y)
-    step = 40 //步长系数 即剩余的距离除以40 每1ms 移动一段距离
-    if (tar_y>current_y){ //tar_y > current_y 即向下滚动
-      var dist = Math.ceil((tar_y-current_y)/step)
-      var next_y = current_y+dist
-      if(next_y<tar_y){  //向上滚动和向下滚动判定的区别 是这里！！
-        window.scrollTo(0,next_y)
-        scroll_To(tar_y)
-        console.log('向下')
-      }
-      else{
-        window.scrollTo(0,tar_y)
-        clearTimeout(timer)
-      }
-    }
-    else{ //tar_y < current_y 即向上滚动
-      var dist = Math.floor((tar_y-current_y)/step)
-      var next_y = current_y+dist
-      if(next_y>tar_y){
-        window.scrollTo(0,next_y)
-        scroll_To(tar_y)
-        console.log('向上')
-      }
-      else{
-        window.scrollTo(0,tar_y)
-        clearInterval(timer)
-      }
-    }
-  },1)
-}
